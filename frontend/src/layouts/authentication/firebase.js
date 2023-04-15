101533f7e38ced5af4782b8310b1884e25f61c60
Rishabh Tripathi <rishabh.201773101@vcet.edu.in>
2023-04-15 16:30:10 +0530

    signup

---
+++ frontend/src/layouts/authentication/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBFVUFb4OxHB5xqgs_Gj9Bks45UN_kkt1s",
+    authDomain: "terra-finance-8927d.firebaseapp.com",
+    projectId: "terra-finance-8927d",
+    storageBucket: "terra-finance-8927d.appspot.com",
+    messagingSenderId: "999929828829",
+    appId: "1:999929828829:web:3c2dcbb15574ab908d9234",
+    measurementId: "G-GSS4D7EXHF"
+};
+ 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export default app;
