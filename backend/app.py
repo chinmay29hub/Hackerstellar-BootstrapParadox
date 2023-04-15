6c4b5f91562e8c29cfe31f962fe60d8e7f09df7a
Thorfinnn <bastaher3@gmail.com>
2023-04-16 04:41:25 +0530

    something

--- backend/app.py
+++ backend/app.py
@@ -19,8 +19,9 @@ for cat in categories:
         models[cat] = pickle.load(file)
 print(models.keys())
 
-def send_mail(receiver_email='barwaniwalataher6@gmail.com',body='Body of the email'):
+def send_mail(receiver_email='barwaniwalataher6@gmail.com',name = "Kshitij",category = "Shopping"):
 # Email account credentials
+    body = "\nDear "+name+",\n\nYour expenditure in "+category+" has exceeded the budget limit set for this category. We urge you to take immediate action to address this situation and bring your spending in line with the budget limit. Failure to do so may have a negative impact on your overall financial position and hinder your ability to achieve your financial goals.\n\nAs a budget monitoring company, we understand the importance of financial prudence and effective resource management. We offer budget monitoring and management solutions that can assist you in managing your finances effectively. Please contact us if you require any assistance.\n\nBest regards,\n\nTaher Barwaniwala\n\nTerraFinances"
     sender_email = 'barwaniwalataher6@outlook.com'
     sender_password = '_Taher@2002'
     receiver_email = receiver_email
@@ -76,4 +77,5 @@ def forecast_spending():
     return json.dumps({"prediction":prediction.tolist()})
 
 if __name__ == '__main__':
+    # send_mail()
     app.run(port=4000)
